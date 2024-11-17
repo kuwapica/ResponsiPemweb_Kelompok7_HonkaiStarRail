68a13f0541f15295bd92bf244b866598b7b3f006
kuwapica <annidaaiska@gmail.com>
2024-11-17 19:33:22 +0700

    refactor koneksi.php and add logout.php functionality

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,3 @@
 <?php
-$servername = "localhost";
-$database = "pemweb_hsr";
-$username = "root";
-$password = "";
-
-$conn = mysqli_connect($servername, $username, $password, $database);
-
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-} else {
-    echo "Koneksi berhasil";
-}
+$koneksi = mysqli_connect("localhost","root","","pemweb_hsr") or die("Koneksi gagal");
 ?>
