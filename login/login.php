73ccd43c5d90c7db68b9a607da120894a754fb9d
Diky Amarulloh <145264618+Dark545-234@users.noreply.github.com>
2024-11-18 08:08:34 +0700

    Add files via upload

---
+++ login/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Halaman Login</title>
+    <link rel="stylesheet" href="style1.css" />
+  </head>
+  <body>
+    <div class="formulir">
+      <header>LOGIN</header>
+      <form action="#">
+        <div><input type="text" id="input_nama" placeholder="Username...." /></div>
+        <div>
+          <input type="text" id="input_password" placeholder="Password...." />
+        </div>
+        <div><input type="submit" value="Login" /></div>
+      </form>
+      <div class="sign-txt">Not yet member? <a href="#">Signup now</a></div>
+    </div>
+  </body>
+</html>
