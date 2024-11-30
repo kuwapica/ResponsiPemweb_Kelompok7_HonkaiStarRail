6eaa25becf032fbdfde913ab20470280ace9d0ce
Diky Amarulloh <145264618+Dark545-234@users.noreply.github.com>
2024-11-30 11:12:32 +0700

    Add files via upload

---
+++ Enemy/Bos3.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Beranda</title>
+    <link rel="stylesheet" href="stylebos.css" />
+  </head>
+  <body>
+    <header class="l-header">
+      <nav class="nav bd-grid">
+        <div>
+          <a href="#" class="nav__logo">Honkai Star rail Manager</a>
+        </div>
+        <div class="nav__menu" id="nav-menu">
+          <ul class="nav__list">
+            <li class="nav__item"><a href="../index.php" class="nav__link">Beranda</a></li>
+            <li class="nav__item">
+              <a href="#" class="nav__link active">Enemy</a>
+            </li>
+            <li class="nav__item">
+              <a href="#" class="nav__link">Character</a>
+            </li>
+            <li class="nav__item">
+              <a href="#" class="nav__link">Light Cone</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    <main>
+      <div class="container">
+        <div class="kanan">
+          <img
+            src="bos 3.png"
+            alt="Maskot"
+            class="kanan__img"
+            style="
+              height: 502px;
+              width: 340px;
+              margin-left: 60px;
+              padding-top: 30px;
+              padding-bottom: 30px;
+            "
+          />
+        </div>
+        <div class="kiri">
+          <div class="judul">
+            <h1>Profile Bos 3</h1>
+          </div>
+          <p>
+            Nama: Cocolia <br>
+            kekuatan: angin dan ice <br>
+            deskripsi singkat: <br>
+            Guardian Belobog adalah penguasa Benteng yang Menjulang, Belobog. Sebagai Guardian, Cocolia telah mewarisi kekuatan suci, memberinya perintah mutlak atas kekuatan es untuk membekukan musuh-musuhnya dalam pertempuran.
+
+          </p>
+        </div>
+      </div>
+    </main>
+    <footer>
+      <nav class="nav bd-grid">
+        <div>
+          <button><a href="Bos2.php">BACK</a></button>
+        </div>
+        <div class="nav__menu" id="nav-menu">
+          <ul class="nav__list">
+            <li class="nav__item">
+              <button><a href="#">Game Rules</a></button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </footer>
+  </body>
+</html>
